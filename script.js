177c60bb7a466c0d3a72cd37eeefa9312a334d87
trivine <logoms796@gmail.com>
2024-01-03 22:25:41 +0530

    This is my portfolio

---
+++ script.js
@@ -0,0 +1,11 @@
+const yellow =document.getElementById('yellow');
+const main = document.querySelector('main');
+const theme = document.getElementById('theme');
+
+// theme.addEventListener('click' ,() => {
+//     document.body.classList.toggle('dark-theme');
+// });
+
+// main.addEventListener('mousemove',(elem) => {
+//     yellow.style = `transform:translate(${elem.clientX-100}px,${elem.clientY-100}px);`;
+// });
